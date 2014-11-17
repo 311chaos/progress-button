555c9d7f455ce2b43a797743f9bd1365e9fefe6e
Aaron Hickman <ahickman@ebay.com>
2014-11-17 15:56:16 -0700

    update to Maths
    Should resolve some of the drawing issues as the button dimentions changed. There is still a small issue with the corners drawing when the border radius is 0.

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ var App = angular.module('App', []);
 App.controller('AppController', ['$scope', function($scope) {
     $scope.test = 'test';
 }]);
+
--- progButtonDir.js
+++ progButtonDir.js
@@ -7,37 +7,56 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
     scope: {
       additionalClasses: "@"
     },
-    template: '<span class="progButton"><canvas id="fill" width="1" height="1" ></canvas><button class="btn" ng-class="additionalClasses" ng-transclude></button></span>',
+    template: '<span class="progButton"><canvas id="fill" width="1" height="1" ></canvas><button class="btn" ng-class="additionalClasses" ng-transclude></button><input type="range" min="0" max ="100" data-ng-model="doneString"/><input type="number" data-ng-model="done" data-ng-change="drawPercentRect({{done}})"/> </span>',
     link: function(scope, element, attrs) {
 
       var internals = {
         tick_length: 15,
         incrementBy: 3,
-        done: 0,
         incrementPromise: null,
         decrementPromise: null,
         button: element.find('button'),
-        lineWidth: attrs.lineWidth || 4,
+        lineWidth: parseInt(attrs.lineWidth) || 4,
         borderColor: attrs.borderColor || 'blue'
       };
+      
+      scope.done = 100;
+      scope.doneString = "100";
 
 
       internals.button.ready(function() {
-        //get shape dimensions
-        var borderRadius = parseInt(window.getComputedStyle(internals.button[0]).getPropertyValue('border-radius').replace('px', '')); //likely only going to work on Firefox and Chrome. Need to implement a more robust solution. http://acuriousanimal.com/blog/2012/07/09/look-mom-no-jquery-getting-all-css-properties-of-a-dom-element-in-pure-javascript/
-        scope.horizLineLength = internals.button[0].offsetWidth - (borderRadius * 2) + internals.lineWidth;
-        scope.vertLineLength = internals.button[0].offsetHeight - (borderRadius * 2) + internals.lineWidth;
-
 
         //need reference to the canvas element
         var canvas = element.find('canvas')[0];
         var width = canvas.attributes['width'].value = internals.button[0].offsetWidth + (internals.lineWidth * 2);
         var height = canvas.attributes['height'].value = internals.button[0].offsetHeight + (internals.lineWidth * 2);
 
+        //setting these values normalizes the values, in display the button would be 71.7798787px tall, but getting the property would return 72px.
+        internals.button[0].style.height = internals.button[0].offsetHeight;
+        internals.button[0].style.width = internals.button[0].offsetWidth;
+
         var ctx = canvas.getContext("2d");
         ctx.lineWidth = internals.lineWidth;
         ctx.strokeStyle = internals.borderColor;
         //ctx.lineCap = 'butt';
+
+        //get shape dimensions
+        var borderRadius = parseInt(window.getComputedStyle(internals.button[0]).getPropertyValue('border-radius').replace('px', '')); //likely only going to work on Firefox and Chrome. Need to implement a more robust solution. http://acuriousanimal.com/blog/2012/07/09/look-mom-no-jquery-getting-all-css-properties-of-a-dom-element-in-pure-javascript/
+        borderRadius = Math.min(borderRadius, internals.button[0].offsetHeight/2);
+        console.log(borderRadius);
+        console.log(internals.button[0].offsetHeight);
+
+        scope.horizLineLength = internals.button[0].offsetWidth - (borderRadius * 2) + internals.lineWidth;
+        scope.vertLineLength = internals.button[0].offsetHeight - (borderRadius * 2) + internals.lineWidth;
+
+        //update the button with the offset from the borderRadius
+        internals.button[0].style.left = internals.lineWidth;
+        internals.button[0].style.top = internals.lineWidth;
+
+
+
+
+
 
         // calc some lengths for use in percent complete
         var cornerLength = 2 * borderRadius * Math.PI;
@@ -58,7 +77,7 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
 
 
         // draw the radius rectangle
-        var drawPercentRect = function(percent) {
+        scope.drawPercentRect = function(percent) {
 
           // percent expressed as a length-traveled-along-rect
           var accumLength = percent / 100 * totalLength;
@@ -72,7 +91,7 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
           if (d > 0) {
             x1 = internals.lineWidth/2 + borderRadius;
             y1 = internals.lineWidth/2;
-            x2 = internals.lineWidth + borderRadius + d;
+            x2 = internals.lineWidth/2 + borderRadius + d;
             y2 = internals.lineWidth/2;
             drawLine(x1, y1, x2, y2);
           }
@@ -81,13 +100,12 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
           d = accumLength - startTR;
           d = Math.min(d, cornerLength);
           if (d > 0) {
-            var x = internals.lineWidth/2 + borderRadius + scope.horizLineLength;
-            var y = internals.lineWidth/2 + borderRadius;
+            var x = canvas.width - (borderRadius + internals.lineWidth);
+            var y = borderRadius + internals.lineWidth;
             var start = -Math.PI * .5;
             var end = -Math.PI * .5 + (d / cornerLength * Math.PI *.5);
             drawCorner(x, y, start, end);
           }
-
 
 
           // right line
@@ -97,7 +115,7 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
             var x1 = internals.lineWidth/2 + borderRadius + scope.horizLineLength + borderRadius;
             var y1 = internals.lineWidth/2 + borderRadius;
             var x2 = internals.lineWidth/2 + borderRadius + scope.horizLineLength + borderRadius;
-            var y2 = internals.lineWidth + borderRadius + d;
+            var y2 = internals.lineWidth/2 + borderRadius + d;
             drawLine(x1, y1, x2, y2);
           }
 
@@ -105,8 +123,8 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
           var d = accumLength - startBR;
           d = Math.min(d, cornerLength);
           if (d > 0) {
-            var x = internals.lineWidth/2 + borderRadius + scope.horizLineLength;
-            var y = internals.lineWidth/2 + borderRadius + scope.vertLineLength;
+            var x = canvas.width - (borderRadius + internals.lineWidth);
+            var y = canvas.height - (borderRadius + internals.lineWidth);
             var start = 0;
             var end = (d / cornerLength) * (Math.PI *.5);
             drawCorner(x, y, start, end);
@@ -127,10 +145,11 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
           var d = accumLength - startBL;
           d = Math.min(d, cornerLength);
           if (d > 0) {
-            var x = internals.lineWidth/2 + borderRadius;
-            var y = internals.lineWidth/2 + borderRadius + scope.vertLineLength;
+            var x = borderRadius + internals.lineWidth;
+            var y = canvas.height - (borderRadius + internals.lineWidth);
             var start = Math.PI / 2;
             var end = (Math.PI * .5) + (d / cornerLength) * (Math.PI * .5);
+
             drawCorner(x, y, start, end);
           }
 
@@ -140,10 +159,9 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
           d = Math.min(d, scope.vertLineLength);
           if (d > 0) {
             var x1 = internals.lineWidth/2;
-            var y1 = internals.lineWidth + borderRadius + scope.vertLineLength;
+            var y1 = internals.lineWidth/2 + borderRadius + scope.vertLineLength;
             var x2 = internals.lineWidth/2;
-            var y2 = borderRadius + scope.vertLineLength - d;
-            console.log(y2);
+            var y2 = borderRadius + scope.vertLineLength + internals.lineWidth/2 - d;
             drawLine(x1, y1, x2, y2);
           }
 
@@ -151,8 +169,8 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
           var d = accumLength - startTL;
           d = Math.min(d, cornerLength);
           if (d > 0) {
-            var x = internals.lineWidth/2 + borderRadius;
-            var y = internals.lineWidth/2 + borderRadius;
+            var x = borderRadius + internals.lineWidth;
+            var y = borderRadius + internals.lineWidth;
             var start = Math.PI;
             var end = Math.PI + (d / cornerLength) * (Math.PI * .5);
             drawCorner(x, y, start, end);
@@ -170,12 +188,13 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
 
         var drawCorner = function(x, y, start, end) {
           ctx.beginPath();
-          ctx.lineWidth = internals.lineWidth *1.25;
-          ctx.arc(x, y, borderRadius, start, end, false);
+          //ctx.lineWidth = internals.lineWidth *1.25;
+          ctx.arc(x, y, borderRadius  + internals.lineWidth/2, start, end, false);
+          //ctx.arc(x, y, borderRadius + internals.lineWidth/2, 0, 2 * Math.PI, false);
           ctx.stroke();
         };
 
-        drawPercentRect(100);
+        scope.drawPercentRect(scope.done);
 
 
 
@@ -201,20 +220,20 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
         };
 
         var incrementProg = function() {
-          internals.done = internals.done + internals.incrementBy;
-          drawPercentRect(internals.done);
+          scope.done = scope.done + internals.incrementBy;
+          scope.drawPercentRect(scope.done);
 
-          if(internals.done >=100) {
-            drawPercentRect(100);
+          if(scope.done >=100) {
+            scope.drawPercentRect(100);
             cancelProgressPromise(internals.incrementPromise);
             internals.button.off('mouseup');
           }
         };
 
         var decrementProg = function() {
-          internals.done = internals.done - internals.incrementBy;
-          if(internals.done > 0)
-            drawPercentRect(internals.done);
+          scope.done = scope.done - internals.incrementBy;
+          if(scope.done > 0)
+            scope.drawPercentRect(scope.done);
           else {
             cancelProgressPromise(internals.decrementPromise);
           }
@@ -224,11 +243,19 @@ App.directive('progressButton', ['$interval', '$timeout', function($interval, $t
           $interval.cancel(promise);
           //the delay here allows for you to visualize the completed progressbar, without this $timeout it looks like the progress only goes to ALMOST done
           $timeout(function(){
-            internals.done = 0;
-            drawPercentRect(internals.done);
+            scope.done = 0;
+            scope.drawPercentRect(scope.done);
           }, 100);
-
         };
+
+        scope.$watch('doneString', function(){
+          scope.done = parseInt(scope.doneString);
+          scope.drawPercentRect(scope.done);
+        });
+
+        scope.$watch('done', function() {
+          scope.doneString = scope.done.toString();
+        });
 
       });
     }
